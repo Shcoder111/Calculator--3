98dada028e9fe258aff3226fc3acc962ba68132e
Shcoder111 <singhhimanshu3003@gmail.com>
2026-06-16 21:27:13 +0530

    initial commit

---
+++ Script.js
@@ -0,0 +1,15 @@
+function getHistory() {
+    return document.getElementById("history-value").innerText;
+}
+
+function printHitory(num) {
+     document.getElementById("history-value").innerText = num;
+}
+
+function getOutput {
+    return document.getElementById("output-value").innerText;
+}
+
+function printOutput {
+    return document.getElementById("output-value").innerText = num;
+}
